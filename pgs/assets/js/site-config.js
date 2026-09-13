3959304175c959418094a52a5236e1da07e8a51a
Toni Hall <tburgess39@gmail.com>
2026-09-12 19:22:12 -0700

    PGS Update

--- pgs/assets/js/site-config.js
+++ pgs/assets/js/site-config.js
@@ -1,3 +1,3 @@
 window.PGS_SITE_CONFIG = {
-  assistantUrl: "https://script.google.com/a/macros/nv.ccsd.net/s/AKfycbzCXcjBhI_sEXx7NkN5lLcHWRxi_P-MQ_MonHsdwV7veeOXrU8INfV8vo_6A0qBtRj8/exec"
+  assistantUrl: "https://script.google.com/a/macros/nv.ccsd.net/s/AKfycbzPptx83L1G4vvKBpG7yvA8Y58cKF9lGXjjrGSkej1z0fiwgKunRF3u-nseJsZ1vp6H/exec"
 };
